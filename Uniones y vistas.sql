b789844afeb36992f39376547a22ff31c22a82ba
Gerson-12 <gersoninteriano12@gmail.com>
2021-10-10 21:25:36 -0600

    Se modificó el archivo, Ejercicio 1

--- Uniones y vistas.sql
+++ Uniones y vistas.sql
@@ -1,1 +1,28 @@
 USE Northwind;
+
+SELECT 
+p.ProductName producto,
+c.CategoryName categoria
+
+FROM Products p
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID;
+
+--EJERCICIO 1
+CREATE VIEW ListadoProductos
+AS
+SELECT 
+p.ProductName producto,
+c.CategoryName categoria,
+s.CompanyName proveedor
+
+
+FROM Products p
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID
+INNER JOIN Shippers s
+ON p.SupplierID = s.ShipperID
+;
+
+--Para consultar la vista
+SELECT producto, categoria, proveedor FROM ListadoProductos;
