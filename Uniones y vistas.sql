cbb9d89bf6392c7666d48cf851a561134a86f9e9
Gerson-12 <gersoninteriano12@gmail.com>
2021-10-10 22:13:09 -0600

    Se modificó el archivo,Ejercicio 2

--- Uniones y vistas.sql
+++ Uniones y vistas.sql
@@ -26,3 +26,23 @@ ON p.SupplierID = s.ShipperID
 
 --Para consultar la vista
 SELECT producto, categoria, proveedor FROM ListadoProductos;
+
+--EJERCICIO 2
+ALTER VIEW ListadoProductos
+AS
+SELECT 
+p.ProductName producto,
+c.CategoryName categoria,
+s.ProductID productosincluidosenlaorden
+
+
+FROM Products p
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID
+LEFT JOIN  dbo.[Order Details] s
+ON OrderID = s.OrderID
+;
+
+SELECT * FROM ListadoProductos;
+
+
